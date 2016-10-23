2eb74bc8715c83bc32c22b5b55aa2dcfc0449d6e
jake <jake.eid.umass@gmail.com>
2016-10-23 18:15:35 +0000

    update

--- server/app.js
+++ server/app.js
@@ -9,9 +9,6 @@ var routes = require('./routes/index');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -19,9 +16,15 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.use('/app', express.static(path.resolve(__dirname, 'app')));
+app.use('/libs', express.static(path.resolve(__dirname, 'libs')));
+
+var renderIndex = (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'index.html'));
+}
+
+app.get('/*', renderIndex);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
