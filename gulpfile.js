1eaccb2a854cb4355c9969760a29d40f94ff78fc
jake <jake.eid.umass@gmail.com>
2016-10-24 14:15:06 +0000

    restructuring to revert to simpler model for ui: jade + jquery

--- gulpfile.js
+++ gulpfile.js
@@ -1,101 +1,14 @@
 var gulp = require('gulp');
-var path = require('path');
-var sourcemaps = require('gulp-sourcemaps');
-var ts = require('gulp-typescript');
-var del = require('del');
-var concat = require('gulp-concat')
-var runSequence = require('run-sequence');
 var nodemon = require('gulp-nodemon')
-var bs = require('browser-sync').create();
 
-
-// SERVER
-gulp.task('clean', function(){
-    return del('dist')
-});
-
-
-
-/*
-  jsNPMDependencies, sometimes order matters here! so becareful!
-*/
-var jsNPMDependencies = [
-    'core-js/client/shim.min.js',
-    'zone.js/dist/zone.js',
-    'reflect-metadata/Reflect.js',
-    'systemjs/dist/system.src.js'
-] 
-
-gulp.task('dependencies:client', function(){
-    var mappedPaths = jsNPMDependencies.map(file => {return path.resolve('node_modules', file)}) 
-    
-    //Let's copy our head dependencies into a dist/libs
-    var copyJsNPMDependencies = gulp.src(mappedPaths, {base:'node_modules'})
-        .pipe(gulp.dest('dist/libs'))
-        
-    var stylesheet = gulp.src('client/styles.css')
-	   .pipe(gulp.dest('dist/app'))
-	
-	var node_modules = gulp.src('node_modules/@angular/**/*.js')
-	   .pipe(gulp.dest('dist/node_modules/@angular'))
-	   
-    node_modules = gulp.src('node_modules/rxjs/**/*.js')
-        .pipe(gulp.dest('dist/node_modules/rxjs'))
-        
-
-});
-
-gulp.task('deploy:client',function(){
-    
-    //Let's copy our index into dist   
-    var copyIndex = gulp.src('client/index.html')
-        .pipe(gulp.dest('dist'))
-        
-    var tsProject = ts.createProject('client/tsconfig.json');
-    var tsResult = gulp.src('client/**/*.ts')
-		.pipe(sourcemaps.init())
-        .pipe(ts(tsProject))
-	return tsResult.js
-        .pipe(sourcemaps.write()) 
-		.pipe(gulp.dest('dist'))
-})
-
-gulp.task('run:source-watch',function(){
-    gulp.watch('./client/**/*.ts',['deploy:client'])
-    gulp.watch('./server/**/*.js',['deploy:server'])
-})
-
-// the real stuff
-gulp.task('run:server', ['build','run:source-watch','nodemon'], function () {
-	gulp.watch('./dist/app', bs.reload);
-	gulp.watch(['./dist/routes/**/*.js', './dist/app.js', './dist/bin/www'], ['bs-delay']);
-});
-
-gulp.task('deploy:server', function(){
-    var serverCode = gulp.src('server/**/*')
-        .pipe(gulp.dest('dist'))
-})
-
-// give nodemon time to restart
-gulp.task('bs-delay', function () {
-  setTimeout(function () {
-    bs.reload({ stream: false });
-  }, 2000);
-});
-
-// our browser-sync config + nodemon chain
-gulp.task('browser-sync', ['nodemon'], function() {
-	bs.init(null, {
-		port: 5000,
-	});
-});
 
 // our gulp-nodemon task
 gulp.task('nodemon', function (cb) {
 	var started = false;
 	return nodemon({
-		script: './dist/bin/www',
-		ext: 'js',
+	    exec: 'node --inspect=5000 --debug-brk ./server/bin/www',
+		script: './server/bin/www',
+		ext: 'js jade',
 		env: {
 			'NODE_ENV': 'development',
 			'DEBUG': 'appname:*'
@@ -117,11 +30,6 @@ gulp.task('nodemon', function (cb) {
 		// handle ctrl+c without a big weep
 		process.exit();
 	});
-});
-
-
-gulp.task('build', function(callback){
-    runSequence('clean','dependencies:client', 'deploy:client', 'deploy:server', callback);
 });
 
 
--- server/app.js
+++ server/app.js
@@ -10,22 +10,20 @@ var routes = require('./routes/index');
 var app = express();
 
 
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/app', express.static(path.resolve(__dirname, 'app')));
-app.use('/libs', express.static(path.resolve(__dirname, 'libs')));
-app.use('/node_modules', express.static(path.resolve(__dirname, 'node_modules')));
-
-var renderIndex = (req,res) => {
-    res.sendFile(path.resolve(__dirname, 'index.html'));
-}
-
-app.get('/*', renderIndex);
+app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
