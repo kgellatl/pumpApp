70f87a9b51fbfa3a33369338180fbdb9c21ce19d
jake <jake.eid.umass@gmail.com>
2016-10-25 00:59:21 +0000

    Finished addition of runGroup functionality

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,13 +3,16 @@ var models  = require('../models');
 
 var router = express.Router();
 var pumpTable = models.pump;
+var modeTable = models.mode;
+var pumpModeRateTable = models.pump_mode_rate;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   pumpTable.findAll().then(function(pumps){
-    var modes = [{ mode_name:'prime beads'}]; 
+    modeTable.findAll().then(function(modes){
     res.render('index', { pumps: pumps, modes: modes });
-  });
+    })
+    });
 });
 
 router.post('/pumps/add',function(req,res,next){
@@ -32,4 +35,21 @@ router.post('/pumps/delete',function(req,res,next){
     });
 })
 
+router.post('/modes/add',function(req,res,next){
+   var payLoad = req.body;
+   var modeName = payLoad["mode_name"];
+   delete payLoad['modeName'];
+   modeTable.create({mode_name: modeName})
+            .then(function(mode){
+                console.log(mode);
+                for(var prop in payLoad){
+                        pumpModeRateTable.create({modeModeName: modeName, pumpPumpName: prop, rate: payLoad[prop]})
+                        .then(function(pumpModeRate){
+                            console.log(pumpModeRate);
+                        });
+                }
+            })
+
+            });
+   
 module.exports = router;
