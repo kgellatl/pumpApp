fccaa4c07ed54f3baa6bf20c95b9f2de90045753
codeGuy401 <jake.eid.umass@gmail.com>
2016-11-03 14:33:50 -0400

    Implemented in-memory volume accumulator. Also, implemented run mode run, via individual run calls to pumps.

--- server/routes/index.js
+++ server/routes/index.js
@@ -92,6 +92,7 @@ router.post('/pumps/updateRate',function(req,res,next){
                 }
                 serialBus.write(output);
 
+                setTimeout("",10000);
                 output = pump.driver_code + "RAT" + "\r";
                 serialBus.write(output);
                 res.end();
